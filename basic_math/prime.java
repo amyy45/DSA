59052ca9b9f19e91cccd96656cd9de66069ba7ef
Sneha Sah <snehasah22@ifheindia.org>
2025-10-05 19:28:54 +0530

    day 10.5

--- basic_math/prime.java
+++ basic_math/prime.java
@@ -1,5 +1,29 @@
 package basic_math;
 
+import java.util.ArrayList;
+
 public class prime {
-    
+    public static boolean isPrime(int n){
+        ArrayList<Integer> divisors = new ArrayList<>();
+        int sqrtN= (int)Math.sqrt(n);
+        for(int i=1;i<sqrtN;i++) {
+            if(n%i==0) {
+                divisors.add(i);
+                if(i!=n/i) {
+                    divisors.add(n/i);
+                }
+            }
+        }
+        return divisors.size()==2;
+    }
+
+    public static void main(String[] args) {
+        int n=29;
+        System.out.println("N: " + n);
+        if(isPrime(n)) {
+            System.out.println(n + " is a prime number");
+        } else {
+            System.out.println(n + " is not a prime number");
+        }
+    }
 }
