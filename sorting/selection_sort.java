6f886aea704422d5bc9dd0a8b928c625c55681cb
Sneha Sah <snehasah22@ifheindia.org>
2025-10-14 15:56:49 +0530

    day 14

--- sorting/selection_sort.java
+++ sorting/selection_sort.java
@@ -4,13 +4,15 @@ public class selection_sort {
     static void selectionSort(int arr[]) {
         int n=arr.length;
         for(int i=0;i<n-1;i++) {
+            int min=i;
             for(int j=i+1;j<n;j++) {
-                if(arr[j]<arr[i]) {
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+                if(arr[j]<arr[min]) {
+                    min=j;
                 }
             }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
         }
     }
     public static void main(String[] args) {
