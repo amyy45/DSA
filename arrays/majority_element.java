5a1b357f1f436117b98976520739c832bff70984
Sneha Sah <snehasah22@ifheindia.org>
2025-11-05 00:09:02 +0530

    day 27

--- arrays/majority_element.java
+++ arrays/majority_element.java
@@ -14,8 +14,24 @@ public class majority_element {
         }
         return -1;
     }
-
-
+    
+    //moore's voting algorithm
+    static int majorityElement2(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        int element=0;
+        for(int i=0;i<n;i++) {
+            if(count==0) {
+                count=1;
+                element=nums[i];
+            } else if(nums[i]==element) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return element;
+    }
     public static void main(String[] args) {
         int[] arr = {4,4,2,4,3,4,4,3,2,4};
         System.out.println("Majority Element: " + majorityElement(arr));
