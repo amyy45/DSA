a4ac16b907a38e938034bdebf440f5d4fb777aab
Sneha Sah <snehasah22@ifheindia.org>
2025-11-04 16:18:10 +0530

    day 25

---
+++ arrays/sort_0s_1s_2s.java
@@ -0,0 +1,34 @@
+package arrays;
+import java.util.*;
+
+public class sort_0s_1s_2s {
+    static void sort012(ArrayList<Integer> arr) {
+        int low=0, mid=0, high=arr.size()-1;
+        while(mid<=high) {
+            if(arr.get(mid)==0) {
+                // swap arr[low] and arr[mid]
+                int temp=arr.get(low);
+                arr.set(low, arr.get(mid));
+                arr.set(mid, temp);
+                low++;
+                mid++;
+            } else if(arr.get(mid)==1) {
+                mid++;
+            } else {
+                // swap arr[mid] and arr[high]
+                int temp=arr.get(mid);
+                arr.set(mid, arr.get(high));
+                arr.set(high, temp);
+                high--;
+            }
+        }
+    }
+
+    static void sort012(int )
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(2,0,1,2,0,1,1,0,2));
+        sort012(arr);
+        System.out.println("Sorted array: " + arr);
+    }
+}
