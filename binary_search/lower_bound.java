b081cfa607a17abcae6a393ba945f968652df5ae
amy45 <snehasah22@ifheindia.org>
2026-01-06 12:46:26 +0530

    day 39

---
+++ binary_search/lower_bound.java
@@ -0,0 +1,24 @@
+public class lower_bound {
+    public static int lowerBound(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        int ans=nums.length; // Default to length if target is greater than all elements
+        int mid=(low+high)/2;
+        while(low<high) {
+            if(nums[mid]>=target) {
+                ans=mid;
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2,2,3};
+        int target = 2;
+        int result = lowerBound(nums, target);
+        System.out.println("The lower bound index of " + target + " is: " + result);
+    }
+}
